4536f3c81593ddd1e67806d3aa6ba1d4692037c7
sama-gharib <sama.gharib-ali-barura@proton.me>
2025-05-02 17:49:40 +0200

    Add network module

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+mod network;
+
+#[macroquad::main("Bored")]
+async fn main() {
+    loop {
+        
+        clear_background(BLACK);
+        draw_text("Work in progress.", 100., 100., 32., WHITE);
+        
+        next_frame().await;
+    }
 }
