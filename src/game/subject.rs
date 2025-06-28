3300528cbc08ec8b1df51a3254b55aa52514fccd
sama-gharib <sama.gharib-ali-barura@proton.me>
2025-06-28 17:04:27 +0200

    Add monster base code

--- src/game/subject.rs
+++ src/game/subject.rs
@@ -1,38 +1,62 @@
 use macroquad::prelude::*;
 
-use crate::utils::Controlable;
+use auto_with::with;
+
+use crate::utils::{Controlable, Dynamic};
 
 use super::keys::*;
 
 #[derive(Debug, Clone)]
+pub enum Brain {
+    Player (KeyBinding),
+    Monster { target: Vec2 }
+}
+
+#[derive(Debug, Clone)]
 pub struct GameSubject {
-    controls: KeyBinding,
     speed: f32,
     
     slide: Vec2,
-    orientation: Vec2
+    orientation: Vec2,
+    
+    brain: Brain
 }
 
 impl Controlable for GameSubject {
     fn handle_events(&mut self) -> bool {
-        self.slide = self.controls.slide.get_vec() * self.speed * get_frame_time();
-        self.orientation = self.controls.look.get_vec();
-                
-        self.slide != Vec2::ZERO || self.orientation != Vec2::ZERO
+        if let Brain::Player(controls) = &self.brain {
+            self.slide = controls.slide.get_vec() * self.speed * get_frame_time();
+            self.orientation = controls.look.get_vec();
+                    
+            self.slide != Vec2::ZERO || self.orientation != Vec2::ZERO
+        } else {
+            false
+        }
     }
 }
+
+impl Dynamic for GameSubject {
+    fn update(&mut self) {
+        if let Brain::Monster { target } = &self.brain {
+            todo!()
+        }
+    }
+}
+
 
 impl Default for GameSubject {
     fn default() -> Self {
         Self {
-            controls: KeyBinding::default(),
             speed: 50.0,
             slide: Vec2::ZERO,
-            orientation: Vec2::ZERO
+            orientation: Vec2::ZERO,
+            brain: Brain::Player ( KeyBinding::default() )
         }
     }
 }
 
 impl GameSubject {
+    with! { brain: Brain }
+    
     pub fn slide(&self) -> Vec2 { self.slide }
 }
--- src/main.rs
+++ src/main.rs
@@ -18,27 +18,4 @@ async fn main() {
     let mut app = Application::default();
     app.run().await;
     println!("\nFinished !");
-    
- 
-}
-
-async fn _map_generation_demo() {
-    let map = Map::generate(50, 50, 0);
-    let mut frame_count: usize = 0;
-    loop{
-        if is_key_pressed(KeyCode::Space) {
-            frame_count = 0;
-        }
-        
-        for room in map.get_rooms_iterator() {
-            for component in room.components.iter() {
-                if let Some(component) = component {
-                    component.draw();
-                }
-            }
-        }
-        
-        frame_count += 1;
-        next_frame().await;
-    }
 }
